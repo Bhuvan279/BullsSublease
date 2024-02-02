1b56bea62a16030fe77f9a16f45a87e867fd130b
Bhuvan <bhuvaneaswar@gmail.com>
2024-02-01 19:26:19 -0500

    dashboard

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors')
 const bodyParser = require('body-parser')
 const app = express()
 const port = 8383
@@ -6,12 +7,21 @@ const {collection, addDoc, getDocs} = require('firebase/firestore')
 
 const {db} = require('./firebase.js')
 
+app.use(cors())
 app.use(bodyParser.json());
 
 app.get('/', async (req, res) => {
     const querySnapshot = await getDocs(collection(db, "rooms"));
     const list = querySnapshot.docs.map((doc) => doc.data())
-    res.send(list)
+    console.log(req.query)
+    const {q, maxPrice, minPrice} = req.query
+
+    const search = (data, maxPrice = Number.MAX_VALUE, minPrice = Number.MIN_VALUE) => {
+        return data.filter((item) => 
+            item.address.toLowerCase().includes(q.toLowerCase()) && item.price >= minPrice
+        );
+    }
+    res.json(search(list, maxPrice, minPrice))
 })
 
 app.post('/addRoom', async (req, res) => {
